b6d02e16a26790381d466eb2be7a89503981d33e
tkdtmy <tkdtmy.academy.0605@gmail.com>
2020-08-14 01:18:34 +0000

    ログイン機能作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   root to: "toppages#index"
   
-  resources :users, only: [:index, :show, :create, :new]
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete 'logout', to: 'sessions#destroy'
+  
   get "signup", to: "users#new"
+  resources :users, only: [:index, :show, :create, :new]
+  
+  
 end
