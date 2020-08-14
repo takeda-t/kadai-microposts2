a727529c0b49b2cbeba0c3c5f448dcd759cba839
tkdtmy <tkdtmy.academy.0605@gmail.com>
2020-08-14 03:27:13 +0000

    メッセージの削除機能

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
   get "signup", to: "users#new"
   resources :users, only: [:index, :show, :create, :new]
   
-  
+  resources :microposts, only: [:create, :destroy]
 end
