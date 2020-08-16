339d382c593d418f7e844ffe7f36914cd8623c4f
tkdtmy <tkdtmy.academy.0605@gmail.com>
2020-08-16 07:54:05 +0000

    フォロー機能作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,10 @@ class ApplicationController < ActionController::Base
     end
   end
   
+  def counts(user)
+    @count_microposts = user.microposts.count
+    @count_followers = user.followers.count
+    @count_followings = user.followings.count
+  end
+  
 end
